5ad0c8b9b218b83952dd56fba57d9fec558e62bc
dimasma0305 <dimasmaulana0305@gmail.com>
2023-10-31 12:50:32 +0800

    add web prototype challenge

---
+++ Web/prototype/dist/server/src/app/api/favorite/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+
+export async function POST(req) {
+    const favorites = {}
+    const account = {}
+
+    const { favorite, reason } = await req.json()
+    if (!favorite || !reason) {
+        return NextResponse.json({ "message": "favorite or reason missing in form body" })
+    }
+    Object.assign(favorites[favorite], reason)
+    if (account.role == "admin") {
+        return NextResponse.json({ flag: process.env.FLAG })
+    }
+    return NextResponse.json(account)
+}
