1086823ff8e0aa9f2b0360592ba75a640fd27f57
dimasma0305 <dimasmaulana0305@gmail.com>
2023-11-26 21:48:29 +0800

    feat: change dir

--- Blockchain/Fallback Fallback Fallback!/outback/images/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-(cd challenge-base && docker build  . -t gcr.io/paradigmxyz/ctf/base:latest)
-(cd eth-challenge-base && docker build  . -t gcr.io/paradigmxyz/ctf/eth-base:latest)
-# (cd cairo-challenge-base && docker build  . -t gcr.io/paradigmxyz/ctf/cairo-base:latest)
